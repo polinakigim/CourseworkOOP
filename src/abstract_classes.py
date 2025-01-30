23163ba477bbf58e9cac81669d064a492882c406
polinakigim <polinakigim@gmail.com>
2025-01-30 18:34:06 +0300

    add hh_api.py and abstract_classes

---
+++ src/abstract_classes.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from src.vacancy_class import Vacancy
+
+class Parser(ABC):
+
+    @abstractmethod
+    def __init__(self, file_worker):
+        pass
+
+    @abstractmethod
+    def load_vacancies(self, keyword):
+        pass
+
+class JSONWorkerABC(ABC):
+    @abstractmethod
+    def add_vacancy(self: object, vacancies: Vacancy) -> None:
+        pass
+
+    @abstractmethod
+    def delete_vacancy(self: object, vacancies: Vacancy) -> None:
+        pass
---
+++ src/hh_api.py
@@ -0,0 +1,25 @@
+from src.abstract_classes import Parser
+import requests
+
+
+class HH(Parser):
+    def __init__(self, file_worker: str = "data/data.json"):
+        """Конструктор обьекта запроса инфо через API сервис"""
+
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__params = {"text": "", "page": 0, "per_page": 100}
+        self.__vacancies = []  # конечный список, в который складываются вакансии list[dict]
+        super().__init__(file_worker)
+
+    def load_vacancies(self, keyword: str):
+        """Метод загрузки данных вакансий из API сервиса"""
+
+        self.__params["text"] = keyword
+        while self.__params.get("page") != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+            vacancies = response.json()["items"]
+            self.__vacancies.extend(vacancies)
+            self.__params["page"] += 1
+
+        return self.__vacancies
